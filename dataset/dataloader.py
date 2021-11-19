83e48aa9bbc86d137e2805d58d34afb74a514cc2
cheungbh <534660436@qq.com>
2021-11-19 14:29:24 +0800

    fix label path

--- dataset/dataloader.py
+++ dataset/dataloader.py
@@ -8,11 +8,11 @@ from collections import Counter
 
 
 class ClassifyDataset(Dataset):
-    def __init__(self, img_path, image_processor=image_normalize, size=224, label_path=""):
+    def __init__(self, img_path, label_cls, image_processor=image_normalize, size=224):
         img_dir_name, img_dir_label, self.img_name, self.img_label = [], [], [], []
         self.size = size
         self.label = []
-        image_label_dict = self.get_labels(img_path, label_path)
+        image_label_dict = label_cls
         # image_label_dict = os.listdir(img_path)
 
         for idx, cls in enumerate(image_label_dict):
@@ -46,18 +46,18 @@ class ClassifyDataset(Dataset):
             _image_object = self.image_processor(image_name, size=self.size)
             return image_name, _image_object, _label
 
-    def get_labels(self, img_dir, label_path):
-        if label_path:
-            return read_labels(label_path)
-        label_path = os.path.join(img_dir, "labels.txt")
-        if os.path.exists(label_path):
-            return read_labels(label_path)
-        else:
-            labels = [cls for cls in os.listdir(img_dir) if os.path.isdir(os.path.join(img_dir, cls))]
-            with open(label_path, "w") as f:
-                for label in labels:
-                    f.write(label + "\n")
-            return labels
+    # def get_labels(self, img_dir, label_path):
+    #     if label_path:
+    #         return read_labels(label_path)
+    #     label_path = os.path.join(img_dir, "labels.txt")
+    #     if os.path.exists(label_path):
+    #         return read_labels(label_path)
+    #     else:
+    #         labels = [cls for cls in os.listdir(img_dir) if os.path.isdir(os.path.join(img_dir, cls))]
+    #         with open(label_path, "w") as f:
+    #             for label in labels:
+    #                 f.write(label + "\n")
+    #         return labels
 
 
 class DataLoader(object):
@@ -65,9 +65,24 @@ class DataLoader(object):
         if adjust_ratio > 0 and phases is ("train", "val"):
             ImgAdjuster(adjust_ratio, data_dir).run()
         assert phases, "Please assign your phases using the dataset!"
-        self.image_datasets = {x: ClassifyDataset(os.path.join(data_dir, x), size=inp_size, label_path=label_path)
-                               for x in phases}
+        self.label = self.get_labels(data_dir, label_path)
+        self.image_datasets = {x: ClassifyDataset(os.path.join(data_dir, x), self.label, size=inp_size) for x in phases}
         self.dataloaders_dict = {x: torch.utils.data.DataLoader(self.image_datasets[x], batch_size=batch_size,
                                                                 shuffle=True, num_workers=num_worker)
                             for x in phases}
         self.cls_num = len(self.image_datasets[phases[0]].label)
+
+    def get_labels(self, img_dir, label_path):
+        if label_path:
+            return read_labels(label_path)
+        label_path = os.path.join(img_dir, "labels.txt")
+        if os.path.exists(label_path):
+            return read_labels(label_path)
+        else:
+            rnd_phase = os.listdir(img_dir)[0]
+            phase_dir = os.path.join(img_dir, rnd_phase)
+            labels = [cls for cls in os.listdir(phase_dir) if os.path.isdir(os.path.join(phase_dir, cls))]
+            with open(label_path, "w") as f:
+                for label in labels:
+                    f.write(label + "\n")
+            return labels
