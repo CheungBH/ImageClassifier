a0140029b998cda8d2dab081132e604950a7c56d
cheungbh <534660436@qq.com>
2021-11-19 14:34:58 +0800

    add color in demo

--- inference.py
+++ inference.py
@@ -3,6 +3,8 @@ from dataset.utils import image_normalize, read_labels, get_pretrain
 from models.build import ModelBuilder
 import cv2
 import config.config as config
+import random
+import torch
 
 label_path = config.label_path
 model_path = config.model_path
@@ -20,15 +22,16 @@ class ModelInference:
         self.MB.load_weight(model_path)
         self.model.eval()
         self.visualize = visualize
+        self.colors = [[random.randint(0, 255) for _ in range(3)] for _ in range(len(self.classes))]
 
     def run(self, img, cnt=0):
         img_tns = image_normalize(img, size=224)
         self.scores = self.MB.inference(img_tns)
-        self.pred_idx = self.scores[0].tolist().index(max(self.scores[0].tolist()))
+        _, self.pred_idx = torch.max(self.scores, 1)
         self.pred_cls = self.classes[self.pred_idx]
         font = cv2.FONT_HERSHEY_SIMPLEX
         if self.visualize:
-            cv2.putText(img, self.pred_cls, (50, 50), font, 2, (0, 0, 255), 3)
+            cv2.putText(img, self.pred_cls, (50, 50), font, 2, self.colors[self.pred_idx], 3)
         return img
 
     def release(self):
