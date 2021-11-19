27ffba8858f3d09c47336c3c4ae34c429348151d
cheungbh <534660436@qq.com>
2021-11-19 14:29:48 +0800

    fix label path

--- dataset/dataloader.py
+++ dataset/dataloader.py
@@ -79,8 +79,7 @@ class DataLoader(object):
         if os.path.exists(label_path):
             return read_labels(label_path)
         else:
-            rnd_phase = os.listdir(img_dir)[0]
-            phase_dir = os.path.join(img_dir, rnd_phase)
+            phase_dir = os.path.join(img_dir, os.listdir(img_dir)[0])
             labels = [cls for cls in os.listdir(phase_dir) if os.path.isdir(os.path.join(phase_dir, cls))]
             with open(label_path, "w") as f:
                 for label in labels:
