1afaf4f0ba7af2903ebc27a8f03b2fe2b37d0756
cheungbh <534660436@qq.com>
2021-11-23 17:45:17 +0800

    add test_logger

--- logger/record.py
+++ logger/record.py
@@ -84,4 +84,9 @@ class TrainRecorder:
 class TestRecorder:
     def __init__(self, args):
         self.metrics = args.metrics
-        self.cls_
+        self.cls_metrics = args.cls_metrics
+        self.cls_num = args.cls_num
+        self.cls_metric_template = [[[] for _ in range(self.cls_num)] for _ in range(len(self.cls_metrics))]
+        best_template = [0 for _ in range(len(self.metrics))]
+
+
