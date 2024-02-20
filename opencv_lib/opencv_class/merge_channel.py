c8792427b7079019c7f4585d60e30860dd5b1d07
cheungbh <cheungbh@cheungbhdeMacBook-Pro.local>
2024-02-20 16:20:52 +0800

    fix merge channel bugs

--- opencv_lib/opencv_class/merge_channel.py
+++ opencv_lib/opencv_class/merge_channel.py
@@ -8,7 +8,7 @@ import cv2 as cv
 processor_dict = {
     "raw": RawImageProcessor,
     "bg": BackgroundExtractor,
-    "optical_flow": OpticalFlowProcessor
+    "optical": OpticalFlowProcessor
 }
 
 
@@ -16,16 +16,17 @@ class MergeChannelProcessor:
 
     def __init__(self, cfg_file):
         base_path = "/".join(cfg_file.split("/")[:-1])
-        cfgs = json.load(cfg_file)
+        with open(cfg_file, "r") as cfg_file:
+            cfgs = json.load(cfg_file)
         sequences, files = cfgs["sequence"], cfgs["files"]
         self.processors = []
 
         for sequence, file in zip(sequences, files):
-            assert sequence in ["raw", "bg", "optical_flow"]
-            self.processors.append(processor_dict[sequence](os.path.join(file, base_path)))
+            assert sequence in ["raw", "bg", "optical"]
+            self.processors.append(processor_dict[sequence](os.path.join(base_path, file)))
 
-    def __call__(self, frame):
+    def __call__(self, frame, view=False):
         gray_imgs = []
         for processor in self.processors:
-            gray_imgs.append(cv.cvtColor(processor(frame), cv.COLOR_BGR2GRAY))
+            gray_imgs.append(cv.cvtColor(processor(frame, view=view), cv.COLOR_BGR2GRAY))
         return cv.merge(tuple(gray_imgs))
--- opencv_lib/opencv_class/raw_image.py
+++ opencv_lib/opencv_class/raw_image.py
@@ -1,7 +1,11 @@
+import cv2 as cv
+
 
 class RawImageProcessor:
     def __init__(self, cfg_file):
         pass
 
-    def __call__(self, frame):
+    def __call__(self, frame, view=False):
+        if view:
+            cv.imshow("raw", frame)
         return frame
